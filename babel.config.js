8849211d7b08baddaed4d61c9a2ca66674a4ae44
ArjandH <152160720+ArjandenHartog@users.noreply.github.com>
2025-08-26 21:12:36 +0200

    Refactor babel.config.js to conditionally include module-resolver plugin and update CI/CD workflow to list current dev dependencies during installation.

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,31 @@
 module.exports = function (api) {
   api.cache(true);
+  
+  // Check if module-resolver plugin is available
+  let moduleResolverPlugin = null;
+  try {
+    require.resolve('babel-plugin-module-resolver');
+    moduleResolverPlugin = [
+      'module-resolver',
+      {
+        alias: {
+          '@': './',
+        },
+      },
+    ];
+  } catch (error) {
+    console.log('babel-plugin-module-resolver not found, path aliases will be handled by Metro resolver');
+  }
+
+  const plugins = [
+    ...(moduleResolverPlugin ? [moduleResolverPlugin] : []),
+    ['react-native-reanimated/plugin', {
+      relativeSourceLocation: true,
+    }],
+  ];
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './',
-          },
-        },
-      ],
-      ['react-native-reanimated/plugin', {
-        relativeSourceLocation: true,
-      }],
-    ],
+    plugins,
   };
 }; 
